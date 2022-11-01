d67dc9f09fe5661865aa23087f518c3b6759db36
edocm <edocm@mailbox.org>
2022-11-01 17:41:49 +0100

    cleanup main.go

--- main.go
+++ main.go
@@ -11,27 +11,4 @@ func init() {
 
 func main() {
 	cmd.Execute()
-
-	// var test map[string]any
-
-	// type Test []struct {
-	// 	Error struct {
-	// 		Type        int    `json:"type"`
-	// 		Address     string `json:"address"`
-	// 		Description string `json:"description"`
-	// 	} `json:"error"`
-	// }
-
-	// var message Test
-
-	// testJson := `[{"error":{"type":101,"address":"","description":"link button not pressed"}}]`
-
-	// json.Unmarshal([]byte(testJson), &message)
-
-	// fmt.Println(message)
-
-	// for key := range test {
-	// 	fmt.Println(fmt.Sprint(test[key]))
-	// }
-
 }
